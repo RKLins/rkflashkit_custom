4bfbae785b0ba3d36232969fe659b5b1407bc9e4
DmitryMurashov <dmtr.murashov@gmail.com>
2023-12-17 18:30:45 +0300

    [:run,prepare_python] Build | Tool for preparing python for running rkflashkit

--- prepare_python.sh
+++ prepare_python.sh
@@ -1,4 +1,4 @@
 sudo python3.7 -m pip install --ignore-installed pycairo
 sudo apt install libgirepository1.0-dev
 sudo python3.7 -m pip install --ignore-installed PyGObject
-sudo python3.7 -m pip install libusb1
+sudo python3.7 -m pip install -U --force libusb1==1.5.0
---
+++ run.sh
@@ -0,0 +1,1 @@
+sudo python3.7 run.py
